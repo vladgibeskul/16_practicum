e92c9c0be1b39fe41d68d79a8ac2733c12187c02
vladgibeskul <wgv96300@gmail.com>
2025-01-29 09:51:33 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2
+{/// <summary>
+/// Задание 1 часть а
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTable();
+            Console.ReadKey();
+        }
+        static void PrintTable()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.Write($"{j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

c855f226061c6eb6fe379af3ad3db3e40a361328
vladgibeskul <wgv96300@gmail.com>
2025-01-29 10:07:16 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTable();
+            Console.ReadKey();
+        }
+
+        static void PrintTable()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                int currentValue = 41 + i * 10;
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write($"{currentValue + j} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

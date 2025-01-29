9f20a16fd0639dbe321fef19a881add46774d35d
vladgibeskul <wgv96300@gmail.com>
2025-01-29 10:57:34 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            PrintTriangle(5);
+        }
+        static void PrintTriangle( int value)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write($"{value} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

581cf1a54c5afd8255534a2e6e56a734aba4ac6c
vladgibeskul <wgv96300@gmail.com>
2025-01-31 14:35:46 +0200

    Add files via upload

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write("# "); 
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}

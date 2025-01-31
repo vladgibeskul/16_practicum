b22ecdc7911348156f7aee169059c5f72bcdb3b5
vladgibeskul <wgv96300@gmail.com>
2025-01-31 13:52:23 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTriangle();
+            Console.ReadKey();
+            
+        }
+        static void PrintTriangle()
+        {
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" 1");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
